a0a6cabc290ec20064d99d405bdb3d1be3916070
Alex Converse <alex.converse@gmail.com>
2025-06-29 21:30:04 -0700

    cleanup default empty buffer

--- src/openfiles.lua
+++ src/openfiles.lua
@@ -1,3 +1,23 @@
+local function cleanup_empty_buffers()
+    for _, buf in ipairs(vim.api.nvim_list_bufs()) do
+        if vim.api.nvim_buf_is_loaded(buf) and vim.api.nvim_buf_is_valid(buf) then
+            local name = vim.api.nvim_buf_get_name(buf)
+            local modified = vim.api.nvim_buf_get_option(buf, 'modified')
+            local lines = vim.api.nvim_buf_get_lines(buf, 0, -1, false)
+
+            local is_empty = name == "" and not modified and (#lines == 0 or (#lines == 1 and lines[1] == ""))
+            if is_empty then
+                local current_buf = vim.api.nvim_get_current_buf()
+                local buf_count = #vim.api.nvim_list_bufs()
+
+                if buf ~= current_buf or buf_count > 1 then
+                    vim.api.nvim_buf_delete(buf, { force = false })
+                end
+            end
+        end
+    end
+end
+
 local function open_glob(edit_func, glob_pattern)
     local files = vim.fn.glob(glob_pattern, false, true)
 
@@ -34,5 +54,7 @@ local function open_files_in_dir(tabs, directory, inputs)
     if original_dir ~= "" then
         vim.fn.chdir(original_dir)
     end
+
+    cleanup_empty_buffers()
 end
 return open_files_in_dir(...)
